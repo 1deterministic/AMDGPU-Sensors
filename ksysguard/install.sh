5b30c082ed8f9c9a5c78ac9316275a15c367bf90
1deterministic <22532656+1deterministic@users.noreply.github.com>
2019-04-28 12:20:10 -0300

    Adding execution permission to the Ksysguard sensors script

--- ksysguard/install.sh
+++ ksysguard/install.sh
@@ -5,5 +5,6 @@ sudo mkdir "/etc/amdgpu-sensors"
 
 echo "installing sensors..."
 sudo cp "amdgpu-sensors.pl" "/etc/amdgpu-sensors"
+sudo chmod +x "/etc/amdgpu-sensors/amdgpu-sensors.pl"
 
 echo "Done! You must now manually add AMDGPU.sgrd in a new tab in Ksysguard (File -> Import tab from file -> $PWD/AMDGPU.sgrd"
